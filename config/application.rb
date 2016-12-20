025bb5d1e3b325672e785133492acdd64bdabbb7
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-20 21:02:09 +0530

    [FEAT] configured rspec

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,15 @@ module OnlineJudge
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
