3477cbe658833c674e17c05c6217e8ea0cfe0469
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-21 21:36:29 +0530

    [FEAT] added devise controller_helper for rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,4 +43,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
+  config.include Warden::Test::Helpers
 end
