6de285f76a96fa2d2687e8587cb6b2697f39785a
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-21 21:36:29 +0530

    [FEAT] added controller helper for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,19 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require_relative 'support/controller_helpers'
+require 'devise'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
+  config.include ControllerHelpers, type: :controller
+  Warden.test_mode!
+
+  config.after do
+    Warden.test_reset!
+  end
+
   config.expect_with :rspec do |expectations|
     # assertions if you prefer.
     # This option will default to `true` in RSpec 4. It makes the `description`
