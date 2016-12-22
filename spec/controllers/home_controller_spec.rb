115a60ba337c80b84f36e17fcb8e1a870c761513
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-22 21:40:48 +0530

    [TEST] added fixtures and test for login

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe 'anonymous user' do
+    before :each do
+      # simulation for anonymous user
+      login_with nil
+    end
 
+    it "should let user see all contest" do
+      get :index
+      expect( response ).to render_template( :index )
+    end
+
+    it "should let user to see all contest" do
+      login_with create( :user )
+      get :index
+      expect( response ).to render_template( :index )
+    end
+  end
 end
