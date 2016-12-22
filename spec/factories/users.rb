1f3530bad83577db4c1e6e3641aafd87e4d3eb0d
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-22 21:40:48 +0530

    [TEST] added fixtures and test for login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
-  factory :user do
-    
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
+
+FactoryGirl.define do
+  factory :user, :class => 'User' do
+    email
+    password '12345678'
+    password_confirmation '12345678'
   end
 end
