9c92ba571640908170bc717f5115265674164c1c
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-22 20:03:37 +0530

    [CHORE] changed depreciated devise::testhelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
 end
