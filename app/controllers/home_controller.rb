9a92ffc8f509bce8f09b067f88eda432d4e1074f
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-23 20:45:36 +0530

    [BUGFIX] fixed state to boolean and mongo queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,8 @@
-require 'bson'
-Moped::BSON = BSON
-
 class HomeController < ApplicationController
   def index
-    upcoming_contests = Contest.where(start_time: { :$gt => DateTime.now })
-    running_contests = Contest.where(start_time: { lte: DateTime.now }, end_time: { gte: DateTime.now })
-    past_contests = Contest.where(end_time: { lt: DateTime.now })
+    upcoming_contests = Contest.where(start_time: { :$gt => DateTime.now }, state: true)
+    running_contests = Contest.where(start_time: { :$lte => DateTime.now }, end_time: { :$gte => DateTime.now }, state: true)
+    past_contests = Contest.where(end_time: { :$lt => DateTime.now }, state: true)
     @Contests = []
     @Contests << { name: "Upcoming Contests" , data: upcoming_contests }
     @Contests << { name: "Running Contests", data: running_contests }
