edbe20aa74c977324722c7bd4d93f00900b45f13
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-24 21:15:23 +0530

    [FEAT] added problems

--- app/controllers/contest_controller.rb
+++ app/controllers/contest_controller.rb
@@ -4,4 +4,9 @@ class ContestController < ApplicationController
     @Contest = Contest.where(ccode: @ccode, state: true).first
     @Problems = @Contest.all_problems
   end
+
+  def problem
+    @pcode = params[:pcode]
+    @Problem = Problem.where(pcode: @pcode, state: true).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'home#index'
   get '/contest/:ccode' => 'contest#index', as: 'contest'
+  get '/contest/:ccode/:pcode' => 'contest#problem', as: 'problem'
 end
