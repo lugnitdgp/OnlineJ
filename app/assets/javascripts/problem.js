059f565b28828e09de556852186946e9094f60a3
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-26 20:00:45 +0530

    [BUGFIX] fixed codemirror with turbolinks

--- app/assets/javascripts/problem.js
+++ app/assets/javascripts/problem.js
@@ -1,12 +1,9 @@
-$(function(){
-  window.codemirror_editors = {};
-  $('.codemirror').each(function(){
-    var $el = $(this);
-    codemirror_editors[$el.attr('id')] = CodeMirror.fromTextArea($el[0],
-      {
-        lineNumbers: true,
-        matchBrackets: true,
-        mode: "text/x-csrc"
-      });
+$(document).on('turbolinks:load', function() {
+  var cEditor = CodeMirror.fromTextArea(document.getElementById("page_body"), {
+     lineNumbers: true,
+     matchBrackets: true,
+     mode: "text/x-csrc"
   });
+  var mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
+  CodeMirror.keyMap.default[(mac ? "Cmd" : "Ctrl") + "-Space"] = "autocomplete";
 });
