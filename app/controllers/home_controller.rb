14b674e175271567cab980d791df72efe1f95903
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-12-27 23:09:39 +0530

    [CHORE] moved db logics to models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    upcoming_contests = Contest.where(start_time: { :$gt => DateTime.now }, state: true)
-    running_contests = Contest.where(start_time: { :$lte => DateTime.now }, end_time: { :$gte => DateTime.now }, state: true)
-    past_contests = Contest.where(end_time: { :$lt => DateTime.now }, state: true)
+    upcoming_contests = Contest.upcomming
+    running_contests = Contest.running
+    past_contests = Contest.past
     @Contests = []
     @Contests << { name: "Upcoming Contests" , data: upcoming_contests }
     @Contests << { name: "Running Contests", data: running_contests }
--- app/models/contest.rb
+++ app/models/contest.rb
@@ -13,6 +13,11 @@ class Contest
   belongs_to :setter, counter_cache: true
   has_and_belongs_to_many :users
 
+  scope :upcomming, -> { where(start_time: { :$gt => DateTime.now }, state: true) }
+  scope :running, -> { where(start_time: { :$lte => DateTime.now }, end_time: { :$gte => DateTime.now }, state: true) }
+  scope :past, -> { where(end_time: { :$lt => DateTime.now }, state: true) }
+  scope :by_code, -> (ccode){ where(ccode: ccode, state: true) }
+
   def all_problems
     problems.where(state: true).order_by(submissions_count: -1)
   end
