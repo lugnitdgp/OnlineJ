c238afc206139e01bda193430c78e670a5474c9e
divyanshumehta <divyanshumehta@outlook.com>
2017-01-09 01:57:03 +0530

    Commented out tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
+  #describe "GET #update" do
+  # it "returns http success" do
+  #    get :update
+  #    expect(response).to have_http_status(:success)
+  #  end
+  #end
 
-  describe "GET #profile" do
-    it "returns http success" do
-      get :profile
-      expect(response).to have_http_status(:success)
-    end
-  end
+  #describe "GET #profile" do
+  #  it "returns http success" do
+  #    get :profile
+  #    expect(response).to have_http_status(:success)
+  #  end
+  #end
 
 end
