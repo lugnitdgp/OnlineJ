63807dd4eaebdd855c01c08b9d402e9b2ab329c2
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-01-08 15:47:51 +0530

    [FEAT] added UI for pagination and modal

--- app/assets/javascripts/local.js
+++ app/assets/javascripts/local.js
@@ -96,31 +96,6 @@ $("select").select2({dropdownCssClass: 'dropdown-inverse'});
       $('[data-toggle="switch"]').bootstrapSwitch();
     }
 
-    // Typeahead
-    if ($('#typeahead-demo-01').length) {
-      var states = new Bloodhound({
-        datumTokenizer: function (d) { return Bloodhound.tokenizers.whitespace(d.word); },
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        limit: 4,
-        local: [
-          { word: 'Alabama' },
-          { word: 'Alaska' },
-          { word: 'Arizona' },
-          { word: 'Arkansas' },
-          { word: 'California' },
-          { word: 'Colorado' }
-        ]
-      });
-
-      states.initialize();
-
-      $('#typeahead-demo-01').typeahead(null, {
-        name: 'states',
-        displayKey: 'word',
-        source: states.ttAdapter()
-      });
-    }
-
     // make code pretty
     window.prettyPrint && prettyPrint();
 
