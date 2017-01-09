67a98a73910b2681f0a8b7445ce735e1db2dc3d9
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-01-09 15:54:58 +0530

    [FEAT] added ui for problems

--- app/controllers/contest_controller.rb
+++ app/controllers/contest_controller.rb
@@ -3,12 +3,17 @@ class ContestController < ApplicationController
   def index
     @ccode = params[:ccode]
     contest = Contest.by_code(@ccode).first
+    if contest.nil?
+      render(file: 'public/404.html', status: :not_found, layout: false) && return
+    end
     @title = contest[:cname]
     problems = contest.all_problems
     @Details = contest[:details]
     lang_data = []
-    problems.each { |problem| lang_data << get_language_data(problem, name: 'name') }
-    @problem_hash = { problems: problems, lang_data: lang_data }
+    success_sub = []
+    problems.each { |problem| lang_data << get_language_data(problem, lang: 'lang_code') }
+    problems.each { |problem| success_sub << problem.submissions.where(status_code: 'AC').count }
+    @problem_hash = { problems: problems, lang_data: lang_data, success_sub: success_sub }
   end
 
   def problem
@@ -29,14 +34,12 @@ class ContestController < ApplicationController
   def get_language_data(model, params = {})
     languages = []
     model.languages.map do |language|
-      languages << if params[:lang]
+      languages << if params[:name]
                      { name: language[params[:name].to_sym], code: language[params[:lang].to_sym] }
                    else
-                     language[params[:name].to_sym]
+                     language[params[:lang].to_sym]
                    end
     end
     languages
   end
-
-  
 end
