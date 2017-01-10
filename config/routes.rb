80c10be50fe9ef7d7ae53d9bb9e1e728378f00ac
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-01-11 02:31:55 +0530

    [FEAT] showing status of job at /statuses

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   authenticate :user, lambda { |user| user.admin? } do
       mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
       mount Sidekiq::Web => '/sidekiq'
+      require 'sidekiq-status/web'
   end
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
