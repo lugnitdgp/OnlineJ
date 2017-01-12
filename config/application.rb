cb5c144f08f88d0fe3b6a2ef10fd2b84f9100c55
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-01-13 02:51:10 +0530

    [FEAT] using mongo caching middleware

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module OnlineJudge
   class Application < Rails::Application
+    config.middleware.use "Mongoid::QueryCache::Middleware"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
