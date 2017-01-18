f9e95af22852bfa78068e1dee8220718800e1b9e
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-01-18 15:27:21 +0530

    [CHORE] changed time creation of main scoreboard

--- app/workers/scoreboard_eval_worker.rb
+++ app/workers/scoreboard_eval_worker.rb
@@ -7,7 +7,7 @@ class ScoreboardEvalWorker
   recurrence { hourly.minute_of_hour(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59) }
 
   def perform
-    contests = Contest.where(start_time: { :$lte => DateTime.now }, end_time: { :$gt => DateTime.now - 10.minutes })
+    contests = Contest.where(start_time: { :$lte => DateTime.now }, end_time: { :$gt => DateTime.now - 5.minutes })
     return if contests.nil?
     contests.each do |contest|
       contest_start_time = contest[:start_time]
