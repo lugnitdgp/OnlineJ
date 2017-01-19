ec351719d6e409b6664a25f7a89e60993f81f534
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-01-20 04:38:09 +0530

    changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(codemirror-load.css codemirror-load.js custom_datatable.css submissions.js users.js later.js)
+Rails.application.config.assets.precompile += %w(codemirror-load.css codemirror-load.js custom_datatable.css submissions.js users.js later.js ckeditor/*)
