a63abe3b93935e30a88bf5b709a8512320fbb962
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-02-18 15:41:54 +0530

    [CHORE] removed mongodb query cache

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ Bundler.require(*Rails.groups)
 module OnlineJudge
   class Application < Rails::Application
     config.time_zone = 'Kolkata'
-    config.middleware.use "Mongoid::QueryCache::Middleware"
-    config.assets.precompile += Ckeditor.assets	
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
