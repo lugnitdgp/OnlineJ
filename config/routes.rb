ba7a7b1674584b2776ca0c7d4c7975df2630e005
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-03-02 20:18:59 +0530

    [FEAT] added edit option in user submission

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/contest/:ccode' => 'contest#index', as: 'contest'
   get '/contest/:ccode/:pcode' => 'contest#problem', as: 'problem'
+  get '/contest/:ccode/:pcode/:submission_id' => 'contest#problem', as: 'edit_submission'
   get '/comments/:ccode/:pcode' => 'contest#comments', as: 'comments'
   post '/submit/:code' => 'submission#verify_submission', as: 'submit'
   get 'submission' => 'submission#index', as: 'submission'
